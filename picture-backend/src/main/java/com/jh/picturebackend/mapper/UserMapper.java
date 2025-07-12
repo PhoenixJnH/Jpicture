378a87ffb3352737bd4a37641b8e03b35023084a
jh <1014547919@qq.com>
2025-07-12 21:35:48 +0800

    v1.1 用户功能开发

---
+++ picture-backend/src/main/java/com/jh/picturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.jh.picturebackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jh.picturebackend.model.entity.User;
+/**
+* @author jh
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-07-11 18:49:16
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User>
+{
+
+}
+
+
+
+
